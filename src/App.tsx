86e7b7f0d77d4dff7463fc6198e73bdbef632c01
Evande1 <crossjab@gmail.com>
2021-12-21 13:31:48 +0800

    'test'

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Box from "@material-ui/core/Box";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
 
-  const addTaskHandler = () => {
-    setTaskList()
-  };
+  // const addTaskHandler = () => {
+  //   setTaskList()
+  // };
 
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
