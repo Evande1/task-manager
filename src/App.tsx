7e0ec5f67267e8de6544ac3d3226c6b55cf2822b
Evande1 <crossjab@gmail.com>
2021-12-21 10:23:59 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  const addTaskHandler = () => {
+    setTaskList()
+  };
+
   return (
     <div className="App">
       <header className="App-header">
